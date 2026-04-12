93ded30b332f2d02d4b7ee4bac5e24d3a5ffe2ce
mark073 <129007199+mark073@users.noreply.github.com>
2026-04-12 11:35:57 +0200

    fix: Uint8Array Blob compatibility in triggerDownload

--- lib/export.ts
+++ lib/export.ts
@@ -9,7 +9,7 @@ function slug(str: string) {
 }
 
 function triggerDownload(content: string | Uint8Array, filename: string, mimeType: string) {
-  const blob = new Blob([content], { type: mimeType })
+  const blob = new Blob([content instanceof Uint8Array ? content.buffer : content], { type: mimeType })
   const url = URL.createObjectURL(blob)
   const a = document.createElement('a')
   a.href = url
